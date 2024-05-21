8642c8733c2e65f7206c51f9bfa0c91f58555c94
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2024-05-20 18:14:33 -0700

    Hello World!

---
+++ README.md
@@ -0,0 +1,3 @@
+# BrainScan (WIP)
+A simple web-based user interface for visualizing interpretations for machine learning models.  
+Okay, let's learn how to use Flask first, shall we? 
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+
+REQUIRED = [
+    'torch',
+    'flask'
+]
+
+with open('README.md', 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name='brainscan',
+    version='0.1.0',
+    description='Library for explaining and visualizing neural networks with a local web-based UI.',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author='Andy S. Yu',
+    author_email='andy.sigeyu@gmail.com',
+    url='',
+    packages=find_packages(),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='GNU GPLv3',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Natural Language :: English',
+    ],
+)
