09c758b006c479f82ec119cc0d8dc57fcf8949a4
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2024-05-25 22:54:36 -0700

    Add upload I guess
    Bro I am so confused about Flask

--- brainscan/app.py
+++ brainscan/app.py
@@ -1,13 +1,38 @@
-
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+app.secret_key = 'DoubleDoubleToilAndTrouble'
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
 
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['image']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('File successfully uploaded')
+        return redirect(url_for('index'))
+    else:
+        flash('Allowed file types are png, jpg, jpeg, gif')
+        return redirect(request.url)
 
 if __name__ == '__main__':
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     app.run(debug=True)
