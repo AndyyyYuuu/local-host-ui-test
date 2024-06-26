ba7eade8a64b9b883bb1d39f2bf56c92e3fb3a35
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2024-06-25 17:07:14 -0700

    feat: Open browser automatically
    Also put port number in variable

--- brainscan/app.py
+++ brainscan/app.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import webbrowser
 import torch
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_socketio import SocketIO, emit
@@ -7,6 +8,8 @@ from flask_socketio import SocketIO, emit
 import threading
 from flask_cors import CORS
 import urllib.parse
+
+PORT = 8000
 
 app = Flask(__name__)
 CORS(app)
@@ -34,17 +37,18 @@ def index():
     return render_template("index.html")
 
 
-def update_bar(title, value: float):
+def update_bar(title: str, value: float):
+    title = urllib.parse.quote(title)
     if type(value) in (int, float):
         statistics["bar"][title] = value
 
         socketio.emit('new_bar_value', {'title': title, 'value': value})
 
     else:
-        raise TypeError("Input parameter `value` must be of type int or float.")
+        raise TypeError("input parameter `value` must be of type int or float.")
 
 
-def update_graph(title, value: float):
+def update_graph(title: str, value: float):
     title = urllib.parse.quote(title)
     if type(value) in (int, float):
         if title in statistics["graph"].keys():
@@ -55,15 +59,15 @@ def update_graph(title, value: float):
         socketio.emit('new_graph_value', {'title': title, 'value': value})
 
     else:
-        raise TypeError("Input parameter `value` must be of type int or float.")
+        raise TypeError("input parameter `value` must be of type int or float.")
 
 
 def run_flask():
-    socketio.run(app, allow_unsafe_werkzeug=True, port=8000)
+    socketio.run(app, allow_unsafe_werkzeug=True, port=PORT)
 
 
 def init(**kwargs):
 
     threading.Thread(target=run_flask).start()
-
+    webbrowser.open_new(f"http://127.0.0.1:{PORT}")
     print("hello")
