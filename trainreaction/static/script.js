521aa3f4ed78ea41584250ec63d0cc14dde2e3ef
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2024-06-28 10:03:00 -0700

    feat: Build basic svg graphs

--- trainreaction/static/script.js
+++ trainreaction/static/script.js
@@ -24,9 +24,38 @@ function updateGraph(title) {
         var newElement = document.createElement('div');
         newElement.id = 'graph-box-'+title;
         newElement.classList.add('graph-box');
-        newElement.innerHTML = "<h2>"+decodeURIComponent(title)+"</h2> <div id='graph-"+title+"'></div>";
+        newElement.innerHTML = `<div class='graph-title'>${decodeURIComponent(title)}</div> <div id='graph-${title}'></div>`;
         graphsContainer.appendChild(newElement);
     }
+    buildLineChart(`graph-${title}`, Array.from({length: values[title].length}, (_, i) => i + 1), values[title]);
+    //Plotly.newPlot('graph-'+title, [trace], {}, {"displayModeBar": false});
+}
 
-    Plotly.newPlot('graph-'+title, [trace], {}, {"displayModeBar": false});
+function buildLineChart(containerId, xData, yData){
+    const width = 23*16;
+    const height = 13*16;
+    const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+
+    const xMin = Math.min(...xData);
+    const xMax = Math.max(...xData);
+    const yMin = Math.min(...yData);
+    const yMax = Math.max(...yData);
+
+    const xScale = d => margin.left + (d - xMin) * (width - margin.left - margin.right) / (xMax - xMin);
+    const yScale = d => height - margin.bottom - (d - yMin) * (height - margin.top - margin.bottom) / (yMax - yMin);
+
+    const lineGenerator = (xData, yData) => {
+        return xData.map((d, i) => {
+            const x = xScale(d);
+            const y = yScale(yData[i]);
+            return `${i === 0 ? 'M' : 'L'} ${x} ${y}`;
+        }).join(' ');
+    };
+
+    var container = document.getElementById(containerId);
+    const linePath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    linePath.setAttribute('class', 'line');
+    linePath.setAttribute('d', lineGenerator(xData, yData));
+    container.innerHTML = `<svg width="25rem" height="15rem" id="${containerId+'-svg'}" class="line-chart"></svg>`
+    document.getElementById(containerId+'-svg').appendChild(linePath)
 }
